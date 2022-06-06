b278a972c0c29160547bfe38c0aa4cb4f564dede
Yan Carlos <urbanoyaan@gmail.com>
2022-06-06 13:12:52 -0500

    Files are added

---
+++ public/js/startup.js
@@ -0,0 +1,32 @@
+let carousel = 1;
+
+let showResposiveMenu = () => {
+    const Local = document.querySelector("#responsiveMenu").classList
+    Local.toggle('absolute')
+    Local.toggle('hidden')
+}
+
+function showSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+showSlides(carousel);
+
+function plusSlides(n) {
+  showSlides(carousel += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {carousel = 1}    
+  if (n < 1) {carousel = slides.length}
+  for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";  
+  }
+  slides[carousel-1].style.display = "block";  
+}
